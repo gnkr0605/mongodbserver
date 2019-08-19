845540324d52545e6d736613cdb10c739682be1e
“naveen” <gnkr0605@gmail.com>
2019-08-20 01:11:30 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+//create the rest object
+let app = require("./config/imports").express();
+//bodyparser module
+let bodyparser = require("./config/imports").bodyparser;
+//set the json as MIME Type
+app.use(bodyparser.json());
+//read the post parameters
+app.use(bodyparser.urlencoded({extended:false}));
+//cors module
+let cors = require("./config/imports").cors;
+//enable the cors
+app.use(cors());
+//use the fetch module
+app.use("/fetch",require("./fetch/fetch"));
+//use insert module
+app.use("/insert",require("./insert/insert"));
+//use update module
+app.use("/update",require("./update/update"));
+//use delete module
+app.use("/delete",require("./delete/delete"));
+//assign the port no
+app.listen(8080);
+console.log("server listening the port no.8080");
